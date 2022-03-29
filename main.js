d3d04df081eeef69c68e41765f462edfcf66eb48
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2022-03-29 14:09:14 -0400

    updates

--- main.js
+++ main.js
@@ -6,7 +6,25 @@ window.onscroll = function(){
     }else{
         header.classList.remove('active');
     }
+
+    if(window.scrollY >= 500){
+        info.classList.add('active');
+    }else{
+        info.classList.remove('active');
+    }
 }
+
+const info = document.querySelector('.sec-info .horizontal-scroll');
+
+// window.onscroll = function(){
+//     if(window.scrollY >= 500){
+//         info.classList.add('active');
+//     }else{
+//         info.classList.remove('active');
+//     }
+// }
+
+const body = document.querySelector('body');
 
 const primaryNav = document.querySelector(".nav-menu");
 const navToggle = document.querySelector(".primary-navigation img");
@@ -14,6 +32,16 @@ const navToggle = document.querySelector(".primary-navigation img");
 const formContent= document.querySelector(".request-form-container");
 const formOpen = document.querySelector(".request-button");
 const formClose = document.querySelector(".close-form");
+
+function toggleForm(){ 
+    const formVisible = formContent.getAttribute('visible');
+    var formIsTrue = (formVisible ==='true');
+    formContent.setAttribute('visible', !formIsTrue);
+
+    const scrollEnabled = body.getAttribute('scroll-enabled');
+    var scrollIsTrue = (scrollEnabled === 'true');
+    body.setAttribute('scroll-enabled', !scrollIsTrue);
+}
 
 navToggle.addEventListener("click", () => {
     const navVisible = primaryNav.getAttribute('visible');
@@ -24,13 +52,10 @@ navToggle.addEventListener("click", () => {
 })
 
 formOpen.addEventListener("click", () => {
-    const formVisible = formContent.getAttribute('visible');
-    var formIsTrue = (formVisible ==='true');
-    formContent.setAttribute('visible', !formIsTrue);
+    window.scrollTo(0,0);
+    toggleForm();
 })
 
 formClose.addEventListener("click", () => {
-    const formVisible = formContent.getAttribute('visible');
-    var formIsTrue = (formVisible ==='true');
-    formContent.setAttribute('visible', !formIsTrue);
+    toggleForm();
 })
